a06e280a3676aa1b55b24867aa3de466283b8eb2
Jefferyzzo <jwang246@usc.edu>
2025-04-07 16:48:53 -0700

    update

--- Baseline/PE/src/MAC.sv
+++ Baseline/PE/src/MAC.sv
@@ -12,29 +12,35 @@ module mac #(
 ) (
     interface  L0,
     interface  L1,
+    interface  L2,
+    interface  L3,
     interface  O     
 ); 
 
-    logic [FILTER_WIDTH*9-1:0] filter;
+    logic [FILTER_WIDTH*3-1:0] filter_row1;
+    logic [FILTER_WIDTH*3-1:0] filter_row2;
+    logic [FILTER_WIDTH*3-1:0] filter_row3;
     logic [9*IFMAP_WIDTH-1:0]  ifmap;
     logic [OUTPUT_WIDTH-1:0] output;
 
     always begin
         fork 
-            L0.Receive(filter);
-            L1.Receieve(ifmap);
+            L0.Receive(filter_row1);
+            L1.Receive(filter_row2);
+            L2.Receive(filter_row3);
+            L3.Receieve(ifmap);
         join
         #FL;
 
-        output = (ifmap[0]? filter[FILTER_WIDTH-1: 0]:16'd0) +
-                 (ifmap[1]? filter[2*FILTER_WIDTH-1:1*FILTER_WIDTH]:16'd0) +
-                 (ifmap[2]? filter[3*FILTER_WIDTH-1:2*FILTER_WIDTH]:16'd0) +
-                 (ifmap[3]? filter[4*FILTER_WIDTH-1:3*FILTER_WIDTH]:16'd0) +
-                 (ifmap[4]? filter[5*FILTER_WIDTH-1:4*FILTER_WIDTH]:16'd0) +
-                 (ifmap[5]? filter[6*FILTER_WIDTH-1:5*FILTER_WIDTH]:16'd0) +
-                 (ifmap[6]? filter[7*FILTER_WIDTH-1:6*FILTER_WIDTH]:16'd0) +
-                 (ifmap[7]? filter[8*FILTER_WIDTH-1:7*FILTER_WIDTH]:16'd0) +
-                 (ifmap[8]? filter[9*FILTER_WIDTH-1:8*FILTER_WIDTH]:16'd0);
+        output = (ifmap[0]? filter_row1[FILTER_WIDTH-1: 0]:16'd0) +
+                 (ifmap[1]? filter_row1[2*FILTER_WIDTH-1:1*FILTER_WIDTH]:16'd0) +
+                 (ifmap[2]? filter_row1[3*FILTER_WIDTH-1:2*FILTER_WIDTH]:16'd0) +
+                 (ifmap[3]? filter_row2[FILTER_WIDTH-1: 0]:16'd0) +
+                 (ifmap[4]? filter_row2[2*FILTER_WIDTH-1:1*FILTER_WIDTH]:16'd0) +
+                 (ifmap[5]? filter_row2[3*FILTER_WIDTH-1:2*FILTER_WIDTH]:16'd0) +
+                 (ifmap[6]? filter_row3[FILTER_WIDTH-1: 0]:16'd0) +
+                 (ifmap[7]? filter_row3[2*FILTER_WIDTH-1:1*FILTER_WIDTH]:16'd0) +
+                 (ifmap[8]? filter_row3[3*FILTER_WIDTH-1:2*FILTER_WIDTH]:16'd0);
         #BL;
         O.Send(output);
     end
