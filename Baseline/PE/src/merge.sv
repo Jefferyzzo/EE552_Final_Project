4e8218f8669b614b277e33f0b18b4b2317354cf7
ychiu078 <ychiu078@usc.edu>
2025-04-07 14:33:23 -0700

    update

--- Baseline/PE/src/merge.sv
+++ Baseline/PE/src/merge.sv
@@ -1,34 +1,33 @@
-//Filling in all the blanks marked with ******************* 
 `timescale 1ns/1ns
 
 import SystemVerilogCSP::*;
 
 module merge #(
-parameter WIDTH	= 8 ,
-parameter FL	= 2 ,
-parameter BL	= 1 
+    parameter WIDTH	= 8,
+    parameter FL	= 2,
+    parameter BL	= 1 
 ) (
-interface  L0   ,//A in the diagram
-interface  L1   ,//B in the diagram
-interface  S    ,
-interface  R     //O in the diagram
+    interface  L0,
+    interface  L1,
+    interface  S,
+    interface  R     
 ); 
 
-logic [WIDTH-1:0] packet;
-logic sel;
+    logic [WIDTH-1:0] packet;
+    logic sel;
 
-
- always begin
- // *******************
-        S.Receive(sel);
-        if (sel == 0) begin
-            L0.Receive(packet);
-			#FL;
-        end else begin
-            L1.Receive(packet);
-			#FL;
-        end
-        R.Send(packet);
-		#BL;
+     always begin
+     // *******************
+            S.Receive(sel);
+            if (sel == 0) begin
+                L0.Receive(packet);
+    			#FL;
+            end else begin
+                L1.Receive(packet);
+    			#FL;
+            end
+            R.Send(packet);
+    		#BL;
     end
+
 endmodule
