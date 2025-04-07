6c0f2237b643a2bf50e1f92f8fc04bfc37ec3c09
ychiu078 <ychiu078@usc.edu>
2025-04-07 15:57:24 -0700

    update

--- Baseline/PE/sim/depacketizer_tb.sv
+++ Baseline/PE/sim/depacketizer_tb.sv
@@ -36,22 +36,20 @@ module data_generator (interface r);
 
 endmodule
 
-module data_bucket (interface r);
+module data_bucket0 (interface r);
     parameter WIDTH = 8;
     parameter BL    = 0; // ideal environment no backward delay
-    // Parameterize the file name for dumping data.
-    parameter string FILENAME = "recv_values_default.txt";
 
     logic [WIDTH-1:0] ReceiveValue = 0;
 
     // File handle for dumping ReceiveValue
     integer recv_file;
 
-    // Open the file at the start of simulation using the parameterized file name.
+    // Open the file at the start of simulation
     initial begin
-        recv_file = $fopen(FILENAME, "w");
+        recv_file = $fopen("recv_timestep.txt", "w");
         if (recv_file == 0) begin
-            $display("Error: Could not open %s for writing.", FILENAME);
+            $display("Error: Could not open recv_timestep.txt for writing.");
             $finish;
         end
     end
@@ -63,7 +61,103 @@ module data_bucket (interface r);
         #BL;
     end
 
-    // Close the file at the end of simulation.
+    // Optionally close the file at the end of simulation.
+    final begin
+        $fclose(recv_file);
+    end
+
+endmodule
+
+module data_bucket1 (interface r);
+    parameter WIDTH = 8;
+    parameter BL    = 0; // ideal environment no backward delay
+
+    logic [WIDTH-1:0] ReceiveValue = 0;
+
+    // File handle for dumping ReceiveValue
+    integer recv_file;
+
+    // Open the file at the start of simulation
+    initial begin
+        recv_file = $fopen("recv_ifmapb_filter.txt", "w");
+        if (recv_file == 0) begin
+            $display("Error: Could not open recv_ifmapb_filter.txt for writing.");
+            $finish;
+        end
+    end
+
+    always begin
+        r.Receive(ReceiveValue);
+        // Dump value to the file along with the simulation time.
+        $fdisplay(recv_file, "Time: %0t - Received Value: %b", $time, ReceiveValue);
+        #BL;
+    end
+
+    // Optionally close the file at the end of simulation.
+    final begin
+        $fclose(recv_file);
+    end
+
+endmodule
+
+module data_bucket2 (interface r);
+    parameter WIDTH = 8;
+    parameter BL    = 0; // ideal environment no backward delay
+
+    logic [WIDTH-1:0] ReceiveValue = 0;
+
+    // File handle for dumping ReceiveValue
+    integer recv_file;
+
+    // Open the file at the start of simulation
+    initial begin
+        recv_file = $fopen("recv_filter_row.txt", "w");
+        if (recv_file == 0) begin
+            $display("Error: Could not open recv_filter_row.txt for writing.");
+            $finish;
+        end
+    end
+
+    always begin
+        r.Receive(ReceiveValue);
+        // Dump value to the file along with the simulation time.
+        $fdisplay(recv_file, "Time: %0t - Received Value: %b", $time, ReceiveValue);
+        #BL;
+    end
+
+    // Optionally close the file at the end of simulation.
+    final begin
+        $fclose(recv_file);
+    end
+
+endmodule
+
+module data_bucket3 (interface r);
+    parameter WIDTH = 8;
+    parameter BL    = 0; // ideal environment no backward delay
+
+    logic [WIDTH-1:0] ReceiveValue = 0;
+
+    // File handle for dumping ReceiveValue
+    integer recv_file;
+
+    // Open the file at the start of simulation
+    initial begin
+        recv_file = $fopen("recv_data.txt", "w");
+        if (recv_file == 0) begin
+            $display("Error: Could not open recv_data.txt for writing.");
+            $finish;
+        end
+    end
+
+    always begin
+        r.Receive(ReceiveValue);
+        // Dump value to the file along with the simulation time.
+        $fdisplay(recv_file, "Time: %0t - Received Value: %b", $time, ReceiveValue);
+        #BL;
+    end
+
+    // Optionally close the file at the end of simulation.
     final begin
         $fclose(recv_file);
     end
@@ -84,10 +178,10 @@ module depacketizer_tb ();
     // Instantiate DUT
     data_generator #(.WIDTH(3*FILTER_WIDTH+4), .FL(0)) dg (Packet);
     depacketizer #(.FILTER_WIDTH(FILTER_WIDTH)) dp (.Packet(Packet), .Timestep(Timestep), .Ifmapb_filter(Ifmapb_filter), .Filter_row(Filter_row), .Data(Data));
-    data_bucket #(.WIDTH(1), .BL(0)) timestep (Timestep);
-    data_bucket #(.WIDTH(1), .BL(0)) ifmapb_filter (Ifmapb_filter);
-    data_bucket #(.WIDTH(2), .BL(0)) filter_row (Filter_row);
-    data_bucket #(.WIDTH(3*FILTER_WIDTH), .BL(0)) data (Data);
+    data_bucket0 #(.WIDTH(1), .BL(0)) timestep (Timestep);
+    data_bucket1 #(.WIDTH(1), .BL(0)) ifmapb_filter (Ifmapb_filter);
+    data_bucket2 #(.WIDTH(2), .BL(0)) filter_row (Filter_row);
+    data_bucket3 #(.WIDTH(3*FILTER_WIDTH), .BL(0)) data (Data);
 
     initial begin
         $display("Start simulation!!!");
